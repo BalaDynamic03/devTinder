0deb985d0d88f55412c5292993733607c6e4455e
TECH-FREAK <145698008+Inbarasan7@users.noreply.github.com>
2026-09-18 23:11:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+const express = require('express'); 
+
+const app = express()
+
+
+app.use("/about", (req, res)=>{
+    res.send("succesfully sending request")
+
+})
+
+app.use("/", (req, res)=>{
+    res.send("succesfully sending main page open starting page")
+
+})
+
+app.listen(7777, ()=>{
+    console.log("sending correctly")
+})
