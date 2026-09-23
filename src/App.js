2d6b06b0cf2e2b750f60ed72e93defa9e1d97845
TECH-FREAK <145698008+Inbarasan7@users.noreply.github.com>
2026-09-23 22:42:03 +0530

    take API

--- src/App.js
+++ src/App.js
@@ -6,18 +6,53 @@ const app = express();
 
 const User = require("./model/user")
 
-app.post("/signup", async (req, res)=>{
-    const user = new User({
-      firstName : "bala",
-      lastName : "bala",
-      emailId:"bala@gmail.com",
-      password: "bala@123"
-    });
+app.use(express.json())
 
-    await user.save()
-    res.send("user Added succesfully")
+app.post("/signup", async (req, res)=>{
+
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("user added successfully...")
+    }catch(err){
+      res.status(400).send("errro saveing" + err.message)
+    }
+    
 })
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const user = await User.findOne({ emailId: userEmail });
+        if(user.length === 0){
+            res.status(400).send("User not found")
+        }else{
+           res.send(user);
+        }
+        
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+
+// feed APi
+app.get("/feed", async (req, res) => {
+    const userEmail = req.query.emailId;
+
+    try {
+        const user = await User.find({ emailId: userEmail });
+        if(user.length === 0){
+            res.status(400).send("User not found")
+        }else{
+           res.send(user);
+        }
+        
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
 
 app.listen(7777, () => {
     console.log("sending correctly");
